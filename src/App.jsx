aefa14ec4c96ebe7fae8702942fd681c3726a88b
doyyseeean <mutiara_angelita_muhaeni@outlook.com>
2024-06-25 14:33:03 +0700

    awal project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar"
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return(
+        <div>
+            <nav className="">test</nav>
+        </div>
+    )
+} // arrow function
+
+export default Navbar;
