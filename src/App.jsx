871445fdac551b7aa5ea3295d7dcf2e138ea5a40
doyyseeean <mutiara_angelita_muhaeni@outlook.com>
2024-06-26 22:24:26 +0700

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Navbar from "./components/Navbar"
 
-
 function App() {
   return (
-    <div>
-      <Navbar/>
-      <h1>Hello World</h1>
+    <div className="overflow-x-hidden text-neutral-300">
+      <div className="fixed top-0 h-full w-full -z-10 absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(120%_125%_at_50%_10%,#000_40%,#241906_70%)]"></div>
+      <div className="mx-auto">
+        <Navbar/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
